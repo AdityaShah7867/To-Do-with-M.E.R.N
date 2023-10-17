bea6088ab81ce51d86e40ce5066e09c1b7b4eabd
Aditya <192105adityashah@gmail.com>
2023-10-17 21:47:36 +0530

    bug fixes

--- server/Server.js
+++ server/Server.js
@@ -1,3 +1,6 @@
+const {initializeApp} = require('firebase/app');
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
+
 var express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -6,12 +9,38 @@ const routes = require("./routes/To-DoRoutes")
 
 const cors = require('cors');
 
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
+
+  const fapp = initializeApp(firebaseConfig)
+
+
+//   .then(()=> console.log('firebase connected !!!'))
+//   .catch((err) => console.log(err));
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+const db = getFirestore(fapp);
+
 
 //MiddleWare
 app.use(cors());
 app.use(express.json());
+
+
+
+
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -19,5 +48,20 @@ mongoose
     .catch((err)=> console.log(err));
 
     app.use("/api", routes);
+
+    async function getfire(db) {
+        const citiesCol = collection(db, 'to-do');
+        const citySnapshot = await getDocs(citiesCol);
+        const cityList = citySnapshot.docs.map(doc => doc.data());
+        console.log(cityList)
+        return cityList
+      } 
+    
+    getfire(db);
+    
+    app.get('/firebase/api/get', async (req,res) => {
+       const data=await getfire(db)
+        res.status(200).json(data)
+      } )
 
 app.listen(PORT, () => {console.log(`Server is running on port: ${PORT}`)});
